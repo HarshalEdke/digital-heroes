e627ce4c1de1406e93727fedf92502453b66e058
HarshalEdke <edkeharshal@gmail.com>
2026-09-22 19:46:41 +0530

    Delete digital-heroes/.next directory

--- digital-heroes/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0cbk-n2._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_0rqfnw9._.js","static/chunks/_1d94q1g._.js"],
-    source: "entry"
-});
